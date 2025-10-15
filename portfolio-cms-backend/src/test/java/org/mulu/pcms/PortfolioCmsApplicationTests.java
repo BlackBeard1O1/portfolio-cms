13c8ca370c4989431acb9ba35455b94dda4d7530
henok <henok608@revature.net>
2025-10-14 20:12:27 -0400

    PCMS-9: Initializing Spring Boot Project

---
+++ portfolio-cms-backend/src/test/java/org/mulu/pcms/PortfolioCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mulu.pcms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
